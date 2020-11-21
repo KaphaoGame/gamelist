0921a3f9f4c47b5c2dc83878fb57eb99f285d429
Nuttawatt Sunthornsatienkul <nuttawatt.s@ku.th>
2020-11-21 17:49:12 +0700

    Remove gameDescription.

--- src/main/java/com/kaphaogame/gamelist/api/GameListModel.java
+++ src/main/java/com/kaphaogame/gamelist/api/GameListModel.java
@@ -2,15 +2,13 @@ package com.kaphaogame.gamelist.api;
 
 public class GameListModel {
     private String gameName;
-    private String gameDescription;
     private String slug;
     private String gameImageURL;
     private Long metacriticScore;
     private Double rating;
 
-    public GameListModel(String gameName, String gameDescription, String slug, String gameImageURL, Long metacriticScore, Double rating) {
+    public GameListModel(String gameName, String slug, String gameImageURL, Long metacriticScore, Double rating) {
         this.gameName = gameName;
-        this.gameDescription = gameDescription;
         this.slug = slug;
         this.gameImageURL = gameImageURL;
         this.metacriticScore = metacriticScore;
@@ -23,8 +21,6 @@ public class GameListModel {
     public String getGameName() {
         return gameName;
     }
-
-    public String getGameDescription() { return gameDescription; }
 
     public String getSlug() {
         return slug;
